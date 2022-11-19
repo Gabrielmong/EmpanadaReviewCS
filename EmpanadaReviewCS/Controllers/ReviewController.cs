26865e2c379199eea57ba66ec9051b9400688dfc
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-11-19 15:37:01 -0600

    feat: initial commit

---
+++ EmpanadaReviewCS/Controllers/ReviewController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmpanadaReviewCS.Controllers
+{
+    public class ReviewController : Controller
+    {
+
+        Models.EmpanadaReviewEntities db = new Models.EmpanadaReviewEntities();
+
+        // GET: Review list page
+        public ActionResult Index() {
+            var reviews = db.Reviews.ToList();
+            return View(reviews);
+        }
+
+
+        // GET: Review
+        public ActionResult Create(Models.ViewModel.ReviewModel review) {
+            
+            if (review == null) {
+                review = new Models.ViewModel.ReviewModel();
+            }
+
+            return View(review);
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult Save(Models.ViewModel.ReviewModel review) {
+            if (!ModelState.IsValid) {
+                return RedirectToAction("Create", review);
+            }
+
+            var newRating = new Models.Rating {
+                score = review.idRating,
+                createdAt = DateTime.Now.Date,
+                updatedAt = DateTime.Now.Date,
+                idRestaurant = review.idRestaurant
+            };
+
+            db.Ratings.Add(newRating);
+            db.SaveChanges();
+
+            var newReview = new Models.Review {
+                idUser = review.idUser,
+                idRating = newRating.idRating,
+                title = review.title,
+                description = review.description,
+                createdAt = DateTime.Now.Date,
+                updatedAt = DateTime.Now.Date,
+                imageSrc = review.imageSrc,
+                idRestaurant = review.idRestaurant,
+                likes = review.likes
+            };
+
+            db.Reviews.Add(newReview);
+            db.SaveChanges();
+            
+            return RedirectToAction("Success", review);
+        }
+
+        public ActionResult Success(Models.ViewModel.ReviewModel review) {
+            return View(review);
+        }
+        
+        public ActionResult Edit(int id) {
+            var review = db.Reviews.Find(id);
+            var reviewModel = new Models.ViewModel.ReviewModel {
+                idReview = review.idReview,
+                idUser = review.idUser,
+                idRating = review.idRating,
+                title = review.title,
+                description = review.description,
+                createdAt = review.createdAt,
+                updatedAt = review.updatedAt,
+                imageSrc = review.imageSrc,
+                idRestaurant = review.idRestaurant,
+                likes = review.likes
+            };
+            return View(reviewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Models.ViewModel.ReviewModel review) {
+
+            var reviewToUpdate = db.Reviews.Find(review.idReview);
+            reviewToUpdate.title = review.title;
+            reviewToUpdate.description = review.description;
+            reviewToUpdate.updatedAt = DateTime.Now.Date;
+            reviewToUpdate.likes = review.likes;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Success", review);
+
+        }
+
+        public ActionResult Delete(int id) {
+            var review = db.Reviews.Find(id);
+            var reviewModel = new Models.ViewModel.ReviewModel {
+                idReview = review.idReview,
+                idUser = review.idUser,
+                idRating = review.idRating,
+                title = review.title,
+                description = review.description,
+                createdAt = review.createdAt,
+                updatedAt = review.updatedAt,
+                imageSrc = review.imageSrc,
+                idRestaurant = review.idRestaurant,
+                likes = review.likes
+            };
+            return View(reviewModel);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteReview(Models.ViewModel.ReviewModel review) {
+            if (!ModelState.IsValid) {
+                return RedirectToAction("Index", review);
+            }
+
+            var reviewToDelete = db.Reviews.Find(review.idReview);
+            db.Reviews.Remove(reviewToDelete);
+            db.SaveChanges();
+
+            return RedirectToAction("Success", review);
+        }
+
+        public ActionResult Details(int id) {
+            var review = db.Reviews.Find(id);
+            var reviewModel = new Models.Review {
+                idReview = review.idReview,
+                idUser = review.idUser,
+                idRating = review.idRating,
+                title = review.title,
+                description = review.description,
+                createdAt = review.createdAt,
+                updatedAt = review.updatedAt,
+                imageSrc = review.imageSrc,
+                idRestaurant = review.idRestaurant,
+                likes = review.likes
+            };
+            return View(reviewModel);
+        }
+        
+        
+    }
+}
---
+++ EmpanadaReviewCS/Models/ViewModel/ReviewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using EmpanadaReviewCS.Models;
+
+namespace EmpanadaReviewCS.Models.ViewModel {
+    public class ReviewModel {
+
+        [Required(ErrorMessage = "Must add an ID")]
+        [Display(Name = "Review ID")]
+        [CodeExists]
+        public int idReview { get; set; }
+
+        [Required(ErrorMessage = "Must add a User ID")]
+        [Display(Name = "User ID")]
+        public int idUser { get; set; }
+
+        [Required(ErrorMessage = "Must add a Rating")]
+        [Display(Name = "Rating")]
+        public int idRating { get; set; }
+
+        [Required(ErrorMessage = "Must add a Title")]
+        [Display(Name = "Title")]
+        [StringLength(30, ErrorMessage = "Title must be less than 30 characters")]
+        public string title { get; set; }
+
+        [Required(ErrorMessage = "Must add a Description")]
+        [Display(Name = "Description")]
+        [StringLength(1024, ErrorMessage = "Description must be less than 1024 characters")]
+        public string description { get; set; }
+
+        [Required(ErrorMessage = "Must add a Created At")]
+        [Display(Name = "Created At")]
+        public System.DateTime createdAt { get; set; }
+
+        [Display(Name = "Updated At")]
+        public Nullable<System.DateTime> updatedAt { get; set; }
+
+        [Display(Name = "Image Source")]
+        public string imageSrc { get; set; }
+
+        [Display(Name = "Restaurant ID")]
+        public Nullable<int> idRestaurant { get; set; }
+
+        [Display(Name = "Likes")]
+        public Nullable<int> likes { get; set; }
+    }
+
+    public class CodeExists : ValidationAttribute {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+            var review = (ReviewModel)validationContext.ObjectInstance;
+            var db = new EmpanadaReviewEntities();
+            var reviewExists = db.Reviews.Any(r => r.idReview == review.idReview);
+            if (reviewExists) {
+                return new ValidationResult("Review ID already exists");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}

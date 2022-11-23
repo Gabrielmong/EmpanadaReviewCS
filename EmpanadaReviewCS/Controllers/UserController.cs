1ec523fb695b096b2d96beccf261f17bfb2f4663
Michael Blanco <michael.blanco0901@gmail.com>
2022-11-23 14:41:43 -0600

    Model and Beginning of Controller

---
+++ EmpanadaReviewCS/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmpanadaReviewCS.Controllers
+{
+    public class UserController : Controller
+    {
+
+        Models.EmpanadaReviewEntities db = new Models.EmpanadaReviewEntities();
+
+        // GET: User list page
+        public ActionResult Index()
+        {
+            return View(db.UserEmpanada.ToList());
+        }
+
+        // GET: User
+        public ActionResult Create(Models.ViewModel.UserModel user)
+        {
+
+            if (user == null)
+            {
+                user = new Models.ViewModel.UserModel();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Save(Models.ViewModel.UserModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Create", user);
+            }
+
+
+            db.UserEmpanada.Add(user);
+            db.SaveChanges();
+
+            return RedirectToAction("Success", user);
+        }
+
+
+
+
+    }
+}
---
+++ EmpanadaReviewCS/Models/ViewModel/UserModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using EmpanadaReviewCS.Models;
+
+namespace EmpanadaReviewCS.Models.ViewModel
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Must add a User ID")]
+        [Display(Name = "User ID")]
+        [CodeExists]
+        public int idUser { get; set; }
+
+        [Required(ErrorMessage = "Must add a Password")]
+        [Display(Name = "Password")]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Must add a First Name")]
+        [Display(Name = "First Name")]
+        [StringLength(15, ErrorMessage = "Title must be less than 15 characters")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Must add a Last Name")]
+        [Display(Name = "Last Name")]
+        [StringLength(15, ErrorMessage = "Title must be less than 15 characters")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Must add a Bio")]
+        [Display(Name = "Bio")]
+        [StringLength(500, ErrorMessage = "Description must be less than 500 characters")]
+        public string bio { get; set; }
+
+        [Required(ErrorMessage = "Must add a Created At")]
+        [Display(Name = "Created At")]
+        public System.DateTime createdAt { get; set; }
+
+        [Display(Name = "Updated At")]
+        public Nullable<System.DateTime> updatedAt { get; set; }
+
+        [Display(Name = "Image Source")]
+        public string imageSrc { get; set; }
+
+        [Required(ErrorMessage = "Must add a Phone Number")]
+        [Display(Name = "Phone Number")]
+        [Range(10000000, 99999999, ErrorMessage = "Phone number must be an 8 digit number")]
+        public int phoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Must add an Email")]     
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [StringLength(320, ErrorMessage = "Email must be less than 320 characters")]
+        public string email { get; set; }
+      
+        [Display(Name = "Gender")]
+        [StringLength(20, ErrorMessage = "Description must be less than 20 characters")]
+        public string gender { get; set; }
+
+        [Display(Name = "Review Quantity")]
+        public Nullable<int> reviews { get; set; }
+
+    }
+}

8460b3474dfd4ac5d1ce72b13e40c1bf022596d4
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-12-03 00:52:48 -0600

    feat: users and security

--- EmpanadaReviewCS/Controllers/HomeController.cs
+++ EmpanadaReviewCS/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using EmpanadaReviewCS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EmpanadaReviewCS.Controllers {
     public class HomeController : Controller {
+
+        private readonly EmpanadaReviewEntities _db = new EmpanadaReviewEntities();
         public ActionResult Index() {
             return View();
         }
@@ -22,5 +26,41 @@ namespace EmpanadaReviewCS.Controllers {
             return View();
         }
 
+        public ActionResult User() {
+            ViewBag.Message = "User page.";
+
+            return View();
+        }
+
+        public ActionResult Login() {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserEmpanada user) {
+            if (ModelState.IsValid) {
+                bool isValid = _db.UserEmpanada.Any(x => x.userName == user.userName && x.password == user.password);
+
+                
+                if (isValid) {
+                    var validUser = _db.UserEmpanada.Where(x => x.userName == user.userName && x.password == user.password).FirstOrDefault();
+                    FormsAuthentication.SetAuthCookie(user.userName, false);
+                    // set the session
+                    Session["userName"] = validUser.userName;
+                    Session["idUser"] = validUser.idUser;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid username or password");
+            return View();
+        }
+
+        public ActionResult Logout() {
+            FormsAuthentication.SignOut();
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
